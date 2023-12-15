3397aad73d9295b34672669cc2c9e1887995338a
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:15:16 +0300

    Update FiguresVector.hpp

--- third_lab/include/containers/FiguresVector.hpp
+++ third_lab/include/containers/FiguresVector.hpp
@@ -1,1 +1,41 @@
+#pragma once
 
+#include "vector.hpp"
+#include "../interfaces/figure.hpp"
+#include <iostream>
+
+class FiguresVector final {
+private:
+    vector<Figure*> _vec;
+
+public:
+    FiguresVector() = default;
+    FiguresVector(const FiguresVector&);
+    FiguresVector(FiguresVector&&) noexcept;
+
+    FiguresVector& operator=(const FiguresVector&);
+    FiguresVector& operator=(FiguresVector&&) noexcept;
+
+    virtual ~FiguresVector() = default;
+
+    void print_geometric_center() const noexcept;
+    void print_area() const noexcept;
+    void print_perimeter() const noexcept;
+    
+    double calculate_total_area() const noexcept;
+
+    void push_back(Figure*);
+    void pop_back();
+    void erase(size_t);
+
+    Figure* operator[](size_t) noexcept;
+    Figure* front() noexcept;
+    Figure* back() noexcept;
+    const Figure* operator[](size_t) const noexcept;
+    const Figure* front() const noexcept;
+    const Figure* back() const noexcept;
+
+    size_t size() const noexcept;
+    size_t capacity() const noexcept;
+    bool empty() const noexcept;
+};
