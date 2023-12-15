62d25f2677f214e7c86ae63ec4531dd08d8df585
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:15:49 +0300

    Update Point.hpp

--- third_lab/include/figures/Point.hpp
+++ third_lab/include/figures/Point.hpp
@@ -1,1 +1,34 @@
+#pragma once
+
+#include <iostream>
+
+class Point final {
+private:
+    double _x;
+    double _y;
+
+public:
+    Point() = default;
+    Point(double, double) noexcept;
+    Point(const std::initializer_list<double>&);
+    Point(const Point&) noexcept;
+    Point(Point&&) noexcept;
+
+    Point& operator=(const Point&) noexcept;
+    Point& operator=(Point&&) noexcept;
+
+    const double get_x_cord() const noexcept;
+    const double get_y_cord() const noexcept;
+
+    void set_x_cord(double) noexcept;
+    void set_y_cord(double) noexcept;
+
+    bool operator==(const Point& other) const noexcept;
+    bool operator!=(const Point& other) const noexcept;
+
+    friend std::istream& operator>>(std::istream&, Point&);
+    friend std::ostream& operator<<(std::ostream&, const Point&);
+
+    virtual ~Point() = default;
+};
 
