f8f1038f94b390981d045a1b2780d00774d5fd65
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:01:05 +0300

    Update tests.cpp

--- first_lab/tests/tests.cpp
+++ first_lab/tests/tests.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "../include/task.hpp"
 
+TEST(task_func_test, random_test_set)
+{
+    ASSERT_TRUE(Solution::task("abc") == "bc");
+}
+
+TEST(task_func_test, only_vowels_test_set)
+{
+    ASSERT_TRUE(Solution::task("aaaaa") == "");
+}
+
+TEST(task_func_test, only_consonants_test_set)
+{
+    ASSERT_TRUE(Solution::task("bbbbbccccc") == "bbbbbccccc");
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
