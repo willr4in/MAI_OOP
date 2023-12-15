29a01739be66934fa544614bb64b4a81502e1bcf
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:17:19 +0300

    Update figure.hpp

--- third_lab/include/interfaces/figure.hpp
+++ third_lab/include/interfaces/figure.hpp
@@ -1,1 +1,20 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include "../figures/Point.hpp"
+
+class Figure {
+protected:
+    Figure() = default;
+
+public:
+    virtual ~Figure() = default;
+
+    virtual Point calculate_geometric_center() const = 0;
+    virtual double calculate_perimeter() const = 0;
+    virtual double calculate_area() const = 0;
+
+    virtual operator double() const = 0;
+};
 
