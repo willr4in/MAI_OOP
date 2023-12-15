f012b54eb793c4784841f75ba87cc80d9824c21c
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:00:21 +0300

    Update task.cpp

--- first_lab/src/task.cpp
+++ first_lab/src/task.cpp
@@ -1,1 +1,13 @@
+#include "../include/task.hpp"
+#include <iostream>
 
+std::string Solution::task(const std::string &str) {
+    std::string res;
+    std::string vowels = "aoeiuyAOEIUY";
+    for (char c : str) {
+        if (vowels.find(c) == std::string::npos) {
+            res.push_back(c);
+        }
+    }
+    return res;
+} 
