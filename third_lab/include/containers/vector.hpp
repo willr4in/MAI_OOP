2065c6745f264556c3fe9ff5e5ae5c17a84a3de4
willr4in <113215579+willr4in@users.noreply.github.com>
2023-12-15 10:15:32 +0300

    Update vector.hpp

--- third_lab/include/containers/vector.hpp
+++ third_lab/include/containers/vector.hpp
@@ -1,1 +1,51 @@
+#pragma once
 
+#include <iostream>
+#include <string>
+
+template <typename T>
+class vector final {
+private:
+    T* _arr;
+    size_t _vec_size;
+    size_t _vec_capacity;
+
+public:
+    vector();
+    vector(const size_t, const T& = T());
+    vector(const std::initializer_list<T>&);
+    vector(const vector<T>&);
+    vector(vector<T>&&) noexcept;
+    virtual ~vector() noexcept;
+
+    vector<T>& operator=(const vector<T>&);
+    vector<T>& operator=(vector<T>&&) noexcept;
+
+    void reserve(const size_t);
+    void resize(const size_t, const T& = T());
+    void shrink_to_fit();
+    void clear();
+
+    void push_back(const T&);
+    template <typename ...Args>
+    void emplace_back(const Args&...);
+    void pop_back();
+
+    void erase(size_t);
+
+    T& operator[](size_t);
+    T& at(size_t);
+    T& front();
+    T& back();
+    const T& operator[](size_t) const;
+    const T& at(size_t) const;
+    const T& front() const;
+    const T& back() const;
+
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
+
+    bool operator==(const vector<T>&) const;
+    bool operator!=(const vector<T>&) const;
+};
